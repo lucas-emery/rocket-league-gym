742746970ef0bdd862631aa07ae3d2c87c20aa7a
Rolv-Arild <rolv_arild@hotmail.com>
2021-04-26 00:56:41 +0200

    Add common rewards (#5)
    * Add single_agent_gym.py
    
    * Add common rewards
    
    * Add common rewards
    
    * Small fix
    
    * Remove single agent env
    
    * Move and add kwargs to SumRewards

---
+++ rlgym/utils/reward_functions/common_rewards.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+from rlgym.utils import RewardFunction, math
+from rlgym.utils.common_values import BLUE_TEAM, ORANGE_GOAL_CENTER, BLUE_GOAL_CENTER
+from rlgym.utils.gamestates import GameState, PlayerData
+
+
+class TouchBallReward(RewardFunction):
+    def reset(self, initial_state: GameState, optional_data=None):
+        pass
+
+    def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray, optional_data=None):
+        if player.ball_touched:
+            return 1
+
+    def get_final_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray, optional_data=None):
+        return 0
+
+
+class MoveTowardsBallReward(RewardFunction):
+    def reset(self, initial_state: GameState, optional_data=None):
+        pass
+
+    def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray, optional_data=None):
+        # Vector version of v=d/t <=> t=d/v <=> 1/t=v/d
+        # Max value should be max_speed / ball_radius = 2300 / 94 = 24.5
+        # Used to guide the agent towards the ball
+        inv_t = math.scalar_projection(player.car_data.linear_velocity, state.ball.position - player.car_data.position)
+        return inv_t
+
+    def get_final_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray, optional_data=None):
+        return 0
+
+
+class GoalReward(RewardFunction):
+    def __init__(self, per_goal: float = 1., team_score_coeff: float = 0., concede_coeff: float = 0.):
+        super().__init__()
+        self.per_goal = per_goal
+        self.team_score_coeff = team_score_coeff
+        self.concede_coeff = concede_coeff
+
+        # Need to keep track of last registered value to detect changes
+        self.goals_scored = 0
+        self.blue_goals = 0
+        self.orange_goals = 0
+
+    def reset(self, initial_state: GameState, optional_data=None):
+        pass
+
+    def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray, optional_data=None):
+        self.goals_scored, d_g = player.match_goals, player.match_goals - self.goals_scored
+        self.blue_goals, d_bg = state.blue_score, state.blue_score - self.blue_goals
+        self.orange_goals, d_og = state.blue_score, state.blue_score - self.orange_goals
+
+        if player.team_num == BLUE_TEAM:
+            return self.per_goal * d_g + self.team_score_coeff * d_bg - self.concede_coeff * d_og
+        else:
+            return self.per_goal * d_g + self.team_score_coeff * d_og - self.concede_coeff * d_bg
+
+    def get_final_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray, optional_data=None):
+        return 0
+
+
+class MoveTowardsGoalReward(RewardFunction):
+    def reset(self, initial_state: GameState, optional_data=None):
+        pass
+
+    def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray, optional_data=None):
+        objective = np.array(ORANGE_GOAL_CENTER if player.team_num == BLUE_TEAM else BLUE_GOAL_CENTER)
+        objective[1] *= 6000 / 5120  # Use back of net instead to prevent exploding reward
+        # Max value should be max_speed / min_dist = 6000 / 786 = 7.6
+        inv_t = math.scalar_projection(state.ball.linear_velocity, objective - player.car_data.position)
+        return inv_t
+
+    def get_final_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray, optional_data=None):
+        return 0
+
+
+class SumRewards(RewardFunction):
+    """
+    For instance, rewarding touch and goal:
+    SumRewards(TouchBallReward(), GoalReward(), coefs=[1, 100])
+    """
+
+    def __init__(self, *reward_functions: RewardFunction, coefs=None):
+        super().__init__()
+        self.reward_funcs = reward_functions
+        self.coefs = [1.] * len(reward_functions) if coefs is None else coefs
+
+    def reset(self, *args, **kwargs):
+        for rew_fn in self.reward_funcs:
+            rew_fn.reset(*args, **kwargs)
+
+    def get_reward(self, *args, **kwargs):
+        return sum(c * rew_fn.get_reward(*args, **kwargs) for rew_fn, c in zip(self.reward_funcs, self.coefs))
+
+    def get_final_reward(self, *args, **kwargs):
+        return sum(c * rew_fn.get_final_reward(*args, **kwargs) for rew_fn, c in zip(self.reward_funcs, self.coefs))
