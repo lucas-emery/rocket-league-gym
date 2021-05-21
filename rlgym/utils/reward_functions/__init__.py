c8d453ade2fc85635ae51a8086b75c0ade788802
Rolv-Arild <rolv_arild@hotmail.com>
2021-05-21 17:59:33 +0200

    Make RewardFunction.get_final_reward return the result of get_reward by default Fix GoalReward Rename some common rewards to make them more clear Add CombinedReward.from_zipped as alternate constructor Add StandStillReward and SaveBoostReward

--- rlgym/utils/reward_functions/__init__.py
+++ rlgym/utils/reward_functions/__init__.py
@@ -2,4 +2,5 @@ from .reward_function import RewardFunction
 from .shoot_ball_reward import ShootBallReward
 from .default_reward import DefaultReward
 from .combined_reward import CombinedReward
-from .common_rewards import TouchBallReward, MoveTowardsBallReward, MoveTowardsGoalReward, GoalReward
+from .common_rewards import TouchBallReward, PlayerTowardsBallReward, BallTowardsGoalReward, GoalReward, \
+    StandStillReward, SaveBoostReward
