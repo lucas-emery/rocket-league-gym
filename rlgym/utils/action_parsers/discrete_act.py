8f376f72a96e804a613907cde91c306b86deafc8
Rolv-Arild <rolv_arild@hotmail.com>
2021-10-12 15:31:20 +0200

    Update discrete_act.py
    Add numpy copy
    Also imagine not using list operators

--- rlgym/utils/action_parsers/discrete_act.py
+++ rlgym/utils/action_parsers/discrete_act.py
@@ -15,10 +15,10 @@ class DiscreteAction(ActionParser):
         self._n_bins = n_bins
 
     def get_action_space(self) -> gym.spaces.Space:
-        return gym.spaces.MultiDiscrete([self._n_bins, self._n_bins, self._n_bins, self._n_bins, self._n_bins, 2, 2, 2])
+        return gym.spaces.MultiDiscrete([self._n_bins] * 5 + [2] * 3)
 
     def parse_actions(self, actions: np.ndarray, state: GameState) -> np.ndarray:
-        actions = actions.reshape((-1, 8))
+        actions = np.copy(actions).reshape((-1, 8))
 
         # map all ternary actions from {0, 1, 2} to {-1, 0, 1}.
         actions[..., :5] = actions[..., :5] / (self._n_bins // 2) - 1
