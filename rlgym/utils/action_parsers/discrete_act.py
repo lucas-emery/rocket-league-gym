7b93ed6ea5d3c3ddbbb5fef094eef0b8ca2b3680
Rolv-Arild <rolv_arild@hotmail.com>
2021-10-14 09:15:05 +0200

    Update discrete_act.py
    Remove copy
    Fix docstring formatting

--- rlgym/utils/action_parsers/discrete_act.py
+++ rlgym/utils/action_parsers/discrete_act.py
@@ -6,7 +6,7 @@ from rlgym.utils.action_parsers import ActionParser
 
 class DiscreteAction(ActionParser):
     """
-        Simple discrete action space. All the analog actions have 3 bins: -1, 0 and 1.
+    Simple discrete action space. All the analog actions have 3 bins by default: -1, 0 and 1.
     """
 
     def __init__(self, n_bins=3):
@@ -18,7 +18,7 @@ class DiscreteAction(ActionParser):
         return gym.spaces.MultiDiscrete([self._n_bins] * 5 + [2] * 3)
 
     def parse_actions(self, actions: np.ndarray, state: GameState) -> np.ndarray:
-        actions = np.copy(actions).reshape((-1, 8))
+        actions = actions.reshape((-1, 8))
 
         # map all ternary actions from {0, 1, 2} to {-1, 0, 1}.
         actions[..., :5] = actions[..., :5] / (self._n_bins // 2) - 1
