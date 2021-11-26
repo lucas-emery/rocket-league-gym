581ddf2ccc11aa8d638061ae65789e28224afafb
Lucas Emery <lucas.emery@hotmail.com>
2021-11-26 20:09:28 +0100

    Improved DefaultObs

--- rlgym/utils/obs_builders/default_obs.py
+++ rlgym/utils/obs_builders/default_obs.py
@@ -1,53 +1,79 @@
 import math
 import numpy as np
+from typing import Any, List
 from rlgym.utils import common_values
 from rlgym.utils.gamestates import PlayerData, GameState
 from rlgym.utils.obs_builders import ObsBuilder
 
 
 class DefaultObs(ObsBuilder):
-    POS_STD = 2300
-    ANG_STD = math.pi
-
-    def __init__(self):
+    def __init__(self, pos_coef=1/2300, ang_coef=1/math.pi, lin_vel_coef=1/2300, ang_vel_coef=1/math.pi):
+        """
+        :param pos_coef: Position normalization coefficient
+        :param ang_coef: Rotation angle normalization coefficient
+        :param lin_vel_coef: Linear velocity normalization coefficient
+        :param ang_vel_coef: Angular velocity normalization coefficient
+        """
         super().__init__()
+        self.POS_COEF = pos_coef
+        self.ANG_COEF = ang_coef
+        self.LIN_VEL_COEF = lin_vel_coef
+        self.ANG_VEL_COEF = ang_vel_coef
 
     def reset(self, initial_state: GameState):
         pass
 
-    def build_obs(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> np.ndarray:
-        players = state.players
+    def build_obs(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> Any:
         if player.team_num == common_values.ORANGE_TEAM:
-            player_car = player.inverted_car_data
+            inverted = True
             ball = state.inverted_ball
+            pads = state.inverted_boost_pads
         else:
-            player_car = player.car_data
+            inverted = False
             ball = state.ball
+            pads = state.boost_pads
 
-        ob = []
+        obs = [ball.position * self.POS_COEF,
+               ball.linear_velocity * self.LIN_VEL_COEF,
+               ball.angular_velocity * self.ANG_VEL_COEF,
+               previous_action,
+               pads]
 
-        ob.append(ball.position / self.POS_STD)
-        ob.append(ball.linear_velocity / self.POS_STD)
-        ob.append(ball.angular_velocity / self.ANG_STD)
+        self._add_player_to_obs(obs, player, inverted)
 
-        ob.append(player_car.position / self.POS_STD)
-        ob.append(player_car.linear_velocity / self.POS_STD)
-        ob.append(player_car.angular_velocity / self.ANG_STD)
-        ob.append([player.boost_amount,
-                   int(player.has_flip),
-                   int(player.on_ground)])
+        allies = []
+        enemies = []
 
-        for other in players:
+        for other in state.players:
             if other.car_id == player.car_id:
                 continue
 
-            if player.team_num == common_values.ORANGE_TEAM:
-                car_data = other.inverted_car_data
+            if other.team_num == player.team_num:
+                team_obs = allies
             else:
-                car_data = other.car_data
+                team_obs = enemies
 
-            ob.append(car_data.position / self.POS_STD)
-            ob.append(car_data.linear_velocity / self.POS_STD)
-            ob.append(car_data.angular_velocity / self.ANG_STD)
-            
-        return np.concatenate(ob)
+            self._add_player_to_obs(team_obs, other, inverted)
+
+        obs.extend(allies)
+        obs.extend(enemies)
+        return np.concatenate(obs)
+
+    def _add_player_to_obs(self, obs: List, player: PlayerData, inverted: bool):
+        if inverted:
+            player_car = player.inverted_car_data
+        else:
+            player_car = player.car_data
+
+        obs.extend([
+            player_car.position * self.POS_COEF,
+            player_car.forward(),
+            player_car.up(),
+            player_car.linear_velocity * self.LIN_VEL_COEF,
+            player_car.angular_velocity * self.ANG_VEL_COEF,
+            [player.boost_amount,
+             int(player.on_ground),
+             int(player.has_flip),
+             int(player.is_demoed)]])
+
+        return player_car
