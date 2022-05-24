5c1ba2da43ea019e4006129941c1d1b6bf142d5f
Rolv-Arild <rolv_arild@hotmail.com>
2022-05-24 09:28:28 +0200

    Add ActionParser to utils init

--- rlgym/utils/__init__.py
+++ rlgym/utils/__init__.py
@@ -2,3 +2,4 @@ from .terminal_conditions import TerminalCondition
 from .reward_functions import RewardFunction
 from .obs_builders import ObsBuilder
 from .state_setters import StateSetter
+from .action_parsers import ActionParser
