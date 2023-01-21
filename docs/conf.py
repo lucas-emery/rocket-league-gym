d8547e3bdfc38f8223d9fc4fe691966d0947b982
George Walter Colgrove IV <georgecolgrove@outlook.com>
2023-01-20 19:54:22 -0500

    Test

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'autoapi.extension']
+extensions = [
+    'sphinx.ext.autodoc', 'autoapi.extension']
 
 autoapi_type = 'python'
 autoapi_dirs = ['../rlgym']
