f41fe8f2c5adca5a88e41ea8492673e3af4f001b
Lucas Emery <lucas.emery@hotmail.com>
2023-08-14 21:10:22 +0200

    QoL done conditions

---
+++ rlgym/rocket_league/done_conditions/all_condition.py
@@ -0,0 +1,25 @@
+from typing import List, Dict, Any, Iterable
+
+from rlgym.api.config.done_condition import DoneCondition
+from rlgym.api.typing import AgentID
+from rlgym.rocket_league.engine.game_state import GameState
+
+
+class AllCondition(DoneCondition[AgentID, GameState]):
+
+    def __init__(self, conditions: Iterable[DoneCondition]):
+        self.conditions = tuple(conditions)
+
+    def reset(self, initial_state: GameState, shared_info: Dict[str, Any]) -> None:
+        for condition in self.conditions:
+            condition.reset(initial_state, shared_info)
+
+    def is_done(self, agents: List[AgentID], state: GameState, shared_info: Dict[str, Any]) -> Dict[AgentID, bool]:
+        # TODO can we optimize this with numpy?
+        combined_dones = {agent: False for agent in agents}
+        for condition in self.conditions:
+            dones = condition.is_done(agents, state, shared_info)
+            for agent in agents:
+                combined_dones[agent] &= dones[agent]
+
+        return combined_dones
--- rlgym/rocket_league/reward_functions/combined_reward.py
+++ rlgym/rocket_league/reward_functions/combined_reward.py
@@ -28,7 +28,7 @@ class CombinedReward(RewardFunction[AgentID, GameState, float]):
 
     def get_rewards(self, agents: List[AgentID], state: GameState, is_terminated: Dict[AgentID, bool],
                     is_truncated: Dict[AgentID, bool], shared_info: Dict[str, Any]) -> Dict[AgentID, float]:
-        # FIXME optimize this double for loop with a numpy matrix
+        # TODO optimize this double for loop with a numpy matrix?
         combined_rewards = {agent: 0. for agent in agents}
         for reward_fn, weight in zip(self.reward_fns, self.weights):
             rewards = reward_fn.get_rewards(agents, state, is_terminated, is_truncated, shared_info)
