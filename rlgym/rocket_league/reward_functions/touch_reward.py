076dd491fa999201c58a2295691c55a1f26f9152
Lucas Emery <lucas.emery@hotmail.com>
2023-08-14 20:40:11 +0200

    Combined and Touch reward

---
+++ rlgym/rocket_league/reward_functions/touch_reward.py
@@ -0,0 +1,17 @@
+from typing import List, Dict, Any
+
+from rlgym.api.config.reward_function import RewardFunction
+from rlgym.api.typing import AgentID
+from rlgym.rocket_league.engine.game_state import GameState
+
+
+class TouchReward(RewardFunction[AgentID, GameState, float]):
+    def reset(self, initial_state: GameState, shared_info: Dict[str, Any]) -> None:
+        pass
+
+    def get_rewards(self, agents: List[AgentID], state: GameState, is_terminated: Dict[AgentID, bool],
+                    is_truncated: Dict[AgentID, bool], shared_info: Dict[str, Any]) -> Dict[AgentID, float]:
+        return {agent: self._get_reward(agent, state) for agent in agents}
+
+    def _get_reward(self, agent: AgentID, state: GameState) -> float:
+        return 1. if state.cars[agent].ball_touches > 0 else 0.
