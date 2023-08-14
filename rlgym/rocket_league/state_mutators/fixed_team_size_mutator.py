a68b1c8fbfc491228bd4f636d7465185dc2f7f0c
Lucas Emery <lucas.emery@hotmail.com>
2023-08-14 20:08:46 +0200

    Move car misc data init to base mutator

--- rlgym/rocket_league/state_mutators/fixed_team_size_mutator.py
+++ rlgym/rocket_league/state_mutators/fixed_team_size_mutator.py
@@ -1,5 +1,7 @@
 from typing import Dict, Any
 
+import numpy as np
+
 from rlgym.api.config.state_mutator import StateMutator
 from rlgym.rocket_league.common_values import BLUE_TEAM, OCTANE, ORANGE_TEAM
 from rlgym.rocket_league.engine.car import Car
@@ -29,5 +31,28 @@ class FixedTeamSizeMutator(StateMutator[GameState]):
     def _new_car(self) -> Car:
         car = Car()
         car.hitbox_type = OCTANE
+
         car.physics = PhysicsObject()
+
+        car.demo_respawn_timer = 0.
+        car.on_ground = True
+        car.supersonic_time = 0.
+        car.boost_amount = 0.
+        car.boost_active_time = 0.
+        car.handbrake = 0.
+
+        car.has_jumped = False
+        car.is_holding_jump = False
+        car.is_jumping = False
+        car.jump_time = 0.
+
+        car.has_flipped = False
+        car.has_double_jumped = False
+        car.air_time_since_jump = 0.
+        car.flip_time = 0.
+        car.flip_torque = np.zeros(3, dtype=np.float32)
+
+        car.is_autoflipping = False
+        car.autoflip_timer = 0.
+        car.autoflip_direction = 0.
         return car
--- rlgym/rocket_league/state_mutators/kickoff_mutator.py
+++ rlgym/rocket_league/state_mutators/kickoff_mutator.py
@@ -5,7 +5,6 @@ import numpy as np
 
 from rlgym.api.config.state_mutator import StateMutator
 from rlgym.rocket_league.common_values import BLUE_TEAM
-from rlgym.rocket_league.engine.car import Car
 from rlgym.rocket_league.engine.game_state import GameState
 
 
@@ -35,32 +34,7 @@ class KickoffMutator(StateMutator[GameState]):
                 orange_count += 1
 
             car.physics.position = pos
+            car.physics.linear_velocity = np.zeros(3, dtype=np.float32)
+            car.physics.angular_velocity = np.zeros(3, dtype=np.float32)
             car.physics.euler_angles = np.array([0, yaw, 0], dtype=np.float32)
             car.boost_amount = 0.33
-            
-            self._fill_other_car_data(car)
-
-    def _fill_other_car_data(self, car: Car):
-        car.physics.linear_velocity = np.zeros(3, dtype=np.float32)
-        car.physics.angular_velocity = np.zeros(3, dtype=np.float32)
-
-        car.demo_respawn_timer = 0.
-        car.on_ground = True
-        car.supersonic_time = 0.
-        car.boost_active_time = 0.
-        car.handbrake = 0.
-
-        car.has_jumped = False
-        car.is_holding_jump = False
-        car.is_jumping = False
-        car.jump_time = 0.
-
-        car.has_flipped = False
-        car.has_double_jumped = False
-        car.air_time_since_jump = 0.
-        car.flip_time = 0.
-        car.flip_torque = np.zeros(3, dtype=np.float32)
-
-        car.is_autoflipping = False
-        car.autoflip_timer = 0.
-        car.autoflip_direction = 0.
