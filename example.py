15c17adee58b060b8379485266c3243214e14497
Lucas Emery <lucas.emery@hotmail.com>
2023-08-15 15:48:45 +0200

    New example

--- example.py
+++ example.py
@@ -1,20 +1,57 @@
-import rlgym
 import time
+import numpy as np
+from itertools import chain
 
-env = rlgym.make()
+from rlgym.api import RLGym
+from rlgym.rocket_league.action_parsers import LookupTableAction, RepeatAction
+from rlgym.rocket_league.done_conditions import GoalCondition, AnyCondition, TimeoutCondition, NoTouchTimeoutCondition
+from rlgym.rocket_league.obs_builders import DefaultObs
+from rlgym.rocket_league.reward_functions import CombinedReward, GoalReward, TouchReward
+from rlgym.rocket_league.sim import RocketSimEngine, RLViserRenderer
+from rlgym.rocket_league.state_mutators import MutatorSequence, FixedTeamSizeMutator, KickoffMutator
+
+
+env = RLGym(
+    state_mutator=MutatorSequence(
+        FixedTeamSizeMutator(blue_size=2, orange_size=2),
+        KickoffMutator()
+    ),
+    obs_builder=DefaultObs(zero_padding=None),
+    action_parser=RepeatAction(LookupTableAction()),
+    reward_fn=CombinedReward(
+        (GoalReward(), 10.),
+        (TouchReward(), 0.1)
+    ),
+    termination_cond=GoalCondition(),
+    truncation_cond=AnyCondition(
+        TimeoutCondition(timeout=300.),
+        NoTouchTimeoutCondition(timeout=30.)
+    ),
+    transition_engine=RocketSimEngine(),
+    renderer=RLViserRenderer()
+)
+
 
 while True:
-    obs = env.reset()
-    done = False
+    obs_dict = env.reset()
     steps = 0
-    ep_reward = 0
+    ep_reward = {agent_id: 0 for agent_id in env.agents}
     t0 = time.time()
-    while not done:
-        actions = env.action_space.sample()  # agent.act(obs) | Your agent should go here
-        new_obs, reward, done, state = env.step(actions)
-        ep_reward += reward
-        obs = new_obs
+    while True:
+        actions = {}
+        for agent_id, action_space in env.action_spaces.items():
+            # agent.act(obs) | Your agent should go here
+            actions[agent_id] = np.random.randint(action_space, size=(1,))
+
+        obs_dict, reward_dict, terminated_dict, truncated_dict = env.step(actions)
+
         steps += 1
+        for agent_id, reward in reward_dict.items():
+            ep_reward[agent_id] += reward
+
+        if any(chain(terminated_dict.values(), truncated_dict.values())):
+            break
 
     length = time.time() - t0
-    print("Step time: {:1.5f} | Episode time: {:.2f} | Episode Reward: {:.2f}".format(length / steps, length, ep_reward))
+    print("Step time: {:1.5f} | Episode time: {:.2f} | Episode Reward: {:.2f}".format(
+        length / steps, length, max(ep_reward.values())))
