67e70156dea28592679b72b53ef1bf30b9d4101f
Alexx <eroszansan@gmail.com>
2023-09-15 14:19:51 +0100

    Include Boost normalization coeff

--- rlgym/utils/obs_builders/advanced_obs.py
+++ rlgym/utils/obs_builders/advanced_obs.py
@@ -1,23 +1,29 @@
 import math
-import numpy as np
 from typing import Any, List
+
+import numpy as np
+
 from rlgym.utils import common_values
-from rlgym.utils.gamestates import PlayerData, GameState, PhysicsObject
+from rlgym.utils.gamestates import GameState, PhysicsObject, PlayerData
 from rlgym.utils.obs_builders import ObsBuilder
 
 
 class AdvancedObs(ObsBuilder):
-    POS_STD = 2300 # If you read this and wonder why, ping Rangler in the dead of night.
+    POS_STD = (
+        2300  # If you read this and wonder why, ping Rangler in the dead of night.
+    )
     ANG_STD = math.pi
 
-    def __init__(self):
+    def __init__(self, std_boost: bool = False):
         super().__init__()
+        self.BOOST_STD = 100 if std_boost else 1
 
     def reset(self, initial_state: GameState):
         pass
 
-    def build_obs(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> Any:
-
+    def build_obs(
+        self, player: PlayerData, state: GameState, previous_action: np.ndarray
+    ) -> Any:
         if player.team_num == common_values.ORANGE_TEAM:
             inverted = True
             ball = state.inverted_ball
@@ -27,11 +33,13 @@ class AdvancedObs(ObsBuilder):
             ball = state.ball
             pads = state.boost_pads
 
-        obs = [ball.position / self.POS_STD,
-               ball.linear_velocity / self.POS_STD,
-               ball.angular_velocity / self.ANG_STD,
-               previous_action,
-               pads]
+        obs = [
+            ball.position / self.POS_STD,
+            ball.linear_velocity / self.POS_STD,
+            ball.angular_velocity / self.ANG_STD,
+            previous_action,
+            pads,
+        ]
 
         player_car = self._add_player_to_obs(obs, player, ball, inverted)
 
@@ -42,43 +50,45 @@ class AdvancedObs(ObsBuilder):
             if other.car_id == player.car_id:
                 continue
 
-            if other.team_num == player.team_num:
-                team_obs = allies
-            else:
-                team_obs = enemies
-
+            team_obs = allies if other.team_num == player.team_num else enemies
             other_car = self._add_player_to_obs(team_obs, other, ball, inverted)
 
             # Extra info
-            team_obs.extend([
-                (other_car.position - player_car.position) / self.POS_STD,
-                (other_car.linear_velocity - player_car.linear_velocity) / self.POS_STD
-            ])
+            team_obs.extend(
+                [
+                    (other_car.position - player_car.position) / self.POS_STD,
+                    (other_car.linear_velocity - player_car.linear_velocity)
+                    / self.POS_STD,
+                ]
+            )
 
         obs.extend(allies)
         obs.extend(enemies)
         return np.concatenate(obs)
 
-    def _add_player_to_obs(self, obs: List, player: PlayerData, ball: PhysicsObject, inverted: bool):
-        if inverted:
-            player_car = player.inverted_car_data
-        else:
-            player_car = player.car_data
-
+    def _add_player_to_obs(
+        self, obs: List, player: PlayerData, ball: PhysicsObject, inverted: bool
+    ):
+        player_car = player.inverted_car_data if inverted else player.car_data
         rel_pos = ball.position - player_car.position
         rel_vel = ball.linear_velocity - player_car.linear_velocity
 
-        obs.extend([
-            rel_pos / self.POS_STD,
-            rel_vel / self.POS_STD,
-            player_car.position / self.POS_STD,
-            player_car.forward(),
-            player_car.up(),
-            player_car.linear_velocity / self.POS_STD,
-            player_car.angular_velocity / self.ANG_STD,
-            [player.boost_amount,
-             int(player.on_ground),
-             int(player.has_flip),
-             int(player.is_demoed)]])
+        obs.extend(
+            [
+                rel_pos / self.POS_STD,
+                rel_vel / self.POS_STD,
+                player_car.position / self.POS_STD,
+                player_car.forward(),
+                player_car.up(),
+                player_car.linear_velocity / self.POS_STD,
+                player_car.angular_velocity / self.ANG_STD,
+                [
+                    player.boost_amount / self.BOOST_STD,
+                    int(player.on_ground),
+                    int(player.has_flip),
+                    int(player.is_demoed),
+                ],
+            ]
+        )
 
         return player_car
--- rlgym/utils/obs_builders/default_obs.py
+++ rlgym/utils/obs_builders/default_obs.py
@@ -1,29 +1,42 @@
 import math
-import numpy as np
 from typing import Any, List
+
+import numpy as np
+
 from rlgym.utils import common_values
-from rlgym.utils.gamestates import PlayerData, GameState
+from rlgym.utils.gamestates import GameState, PlayerData
 from rlgym.utils.obs_builders import ObsBuilder
 
 
 class DefaultObs(ObsBuilder):
-    def __init__(self, pos_coef=1/2300, ang_coef=1/math.pi, lin_vel_coef=1/2300, ang_vel_coef=1/math.pi):
+    def __init__(
+        self,
+        pos_coef=1 / 2300,
+        ang_coef=1 / math.pi,
+        lin_vel_coef=1 / 2300,
+        ang_vel_coef=1 / math.pi,
+        boost_coef: float = 1,
+    ):
         """
         :param pos_coef: Position normalization coefficient
         :param ang_coef: Rotation angle normalization coefficient
         :param lin_vel_coef: Linear velocity normalization coefficient
         :param ang_vel_coef: Angular velocity normalization coefficient
+        :param boost_coef: Boost normalization coefficient
         """
         super().__init__()
         self.POS_COEF = pos_coef
         self.ANG_COEF = ang_coef
         self.LIN_VEL_COEF = lin_vel_coef
         self.ANG_VEL_COEF = ang_vel_coef
+        self.BOOST_COEF = boost_coef
 
     def reset(self, initial_state: GameState):
         pass
 
-    def build_obs(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> Any:
+    def build_obs(
+        self, player: PlayerData, state: GameState, previous_action: np.ndarray
+    ) -> Any:
         if player.team_num == common_values.ORANGE_TEAM:
             inverted = True
             ball = state.inverted_ball
@@ -33,11 +46,13 @@ class DefaultObs(ObsBuilder):
             ball = state.ball
             pads = state.boost_pads
 
-        obs = [ball.position * self.POS_COEF,
-               ball.linear_velocity * self.LIN_VEL_COEF,
-               ball.angular_velocity * self.ANG_VEL_COEF,
-               previous_action,
-               pads]
+        obs = [
+            ball.position * self.POS_COEF,
+            ball.linear_velocity * self.LIN_VEL_COEF,
+            ball.angular_velocity * self.ANG_VEL_COEF,
+            previous_action,
+            pads,
+        ]
 
         self._add_player_to_obs(obs, player, inverted)
 
@@ -48,11 +63,7 @@ class DefaultObs(ObsBuilder):
             if other.car_id == player.car_id:
                 continue
 
-            if other.team_num == player.team_num:
-                team_obs = allies
-            else:
-                team_obs = enemies
-
+            team_obs = allies if other.team_num == player.team_num else enemies
             self._add_player_to_obs(team_obs, other, inverted)
 
         obs.extend(allies)
@@ -60,20 +71,21 @@ class DefaultObs(ObsBuilder):
         return np.concatenate(obs)
 
     def _add_player_to_obs(self, obs: List, player: PlayerData, inverted: bool):
-        if inverted:
-            player_car = player.inverted_car_data
-        else:
-            player_car = player.car_data
-
-        obs.extend([
-            player_car.position * self.POS_COEF,
-            player_car.forward(),
-            player_car.up(),
-            player_car.linear_velocity * self.LIN_VEL_COEF,
-            player_car.angular_velocity * self.ANG_VEL_COEF,
-            [player.boost_amount,
-             int(player.on_ground),
-             int(player.has_flip),
-             int(player.is_demoed)]])
+        player_car = player.inverted_car_data if inverted else player.car_data
+        obs.extend(
+            [
+                player_car.position * self.POS_COEF,
+                player_car.forward(),
+                player_car.up(),
+                player_car.linear_velocity * self.LIN_VEL_COEF,
+                player_car.angular_velocity * self.ANG_VEL_COEF,
+                [
+                    player.boost_amount * self.BOOST_COEF,
+                    int(player.on_ground),
+                    int(player.has_flip),
+                    int(player.is_demoed),
+                ],
+            ]
+        )
 
         return player_car
