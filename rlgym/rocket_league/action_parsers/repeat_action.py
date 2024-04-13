6f06181c559ffe800f195d564d56ff8a4ec23b99
lucas <lucas.emery@hotmail.com>
2024-04-13 15:50:59 +0200

    Improve RepeatAction Now your action parser doesn't have to handle the multiple ticks complexity

--- rlgym/rocket_league/action_parsers/repeat_action.py
+++ rlgym/rocket_league/action_parsers/repeat_action.py
@@ -2,17 +2,17 @@ from typing import Dict, Any
 
 import numpy as np
 
-from rlgym.api import ActionParser, SpaceType, AgentID
+from rlgym.api import ActionParser, ActionType, SpaceType, AgentID
 from rlgym.rocket_league.api import GameState
 
 
-class RepeatAction(ActionParser[AgentID, np.ndarray, np.ndarray, GameState, SpaceType]):
+class RepeatAction(ActionParser[AgentID, ActionType, np.ndarray, GameState, SpaceType]):
     """
     A simple wrapper to emulate tick skip
     """
 
     def __init__(self,
-                 parser: ActionParser[AgentID, np.ndarray, np.ndarray, GameState, SpaceType],
+                 parser: ActionParser[AgentID, ActionType, np.ndarray, GameState, SpaceType],
                  repeats=8):
         super().__init__()
         self.parser = parser
@@ -24,12 +24,15 @@ class RepeatAction(ActionParser[AgentID, np.ndarray, np.ndarray, GameState, Spac
     def reset(self, initial_state: GameState, shared_info: Dict[str, Any]) -> None:
         pass
 
-    def parse_actions(self, actions: Dict[AgentID, np.ndarray], state: GameState, shared_info: Dict[str, Any]) -> Dict[AgentID, np.ndarray]:
+    def parse_actions(self, actions: Dict[AgentID, ActionType], state: GameState, shared_info: Dict[str, Any]) -> Dict[AgentID, np.ndarray]:
+        rlgym_actions = self.parser.parse_actions(actions, state, shared_info)
         repeat_actions = {}
-        for agent, action in actions.items():
-            # Action can have shape (ActionSpace)
-            assert len(action.shape) == 1
+        for agent, action in rlgym_actions.items():
+            if action.shape == (8,):
+                action = np.expand_dims(action, axis=0)
+            elif action.shape != (1, 8):
+                raise ValueError(f"Expected action to have shape (8,) or (1,8), got {action.shape}")
 
-            repeat_actions[agent] = np.expand_dims(action, axis=0).repeat(self.repeats, axis=0)
+            repeat_actions[agent] = action.repeat(self.repeats, axis=0)
 
-        return self.parser.parse_actions(repeat_actions, state, shared_info)
+        return repeat_actions
